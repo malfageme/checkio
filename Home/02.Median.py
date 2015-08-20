82bfca42873fc2f3d99fa693b17758d08d25b6e1
Miguel Alfageme <malfageme@gmail.com>
2015-08-20 15:35:40 +0200

    Second test

---
+++ Home/02.Median.py
@@ -0,0 +1,15 @@
+def checkio(data):
+    if len(data) % 2 == 0:
+        return sum(sorted(data)[len(data)/2-1:len(data)/2+1])/2.0
+    else:
+        return sorted(data)[len(data)/2]
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio([1, 2, 3, 4, 5]) == 3, "Sorted list"
+    assert checkio([3, 1, 2, 5, 3]) == 3, "Not sorted list"
+    assert checkio([1, 300, 2, 200, 1]) == 2, "It's not an average"
+    assert checkio([3, 6, 20, 99, 10, 15]) == 12.5, "Even length"
+    print("Start the long test")
+    assert checkio(range(1000000)) == 499999.5, "Long."
+    print("The local tests are done.")
