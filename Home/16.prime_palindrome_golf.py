c8a20aa0191653948acebda6c8ff708283ca1f86
Miguel Alfageme <malfageme@gmail.com>
2015-08-21 10:48:48 +0200

    Prime palindrome golf space trimming

--- Home/16.prime_palindrome_golf.py
+++ Home/16.prime_palindrome_golf.py
@@ -1,2 +1,2 @@
 def golf2(n):
-    return [i for i in range(n+1,9999) if  str(i)==str(i)[::-1]and all([i%x for x in range(2,i)])][0]
+    return[i for i in range(n+1,99999)if str(i)==str(i)[::-1]and all([i%x for x in range(2,i)])][0]
