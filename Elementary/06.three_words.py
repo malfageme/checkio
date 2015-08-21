3404365c75cf355f6b73adc7fa2532c6e68359e9
Miguel Alfageme <malfageme@gmail.com>
2015-08-21 13:56:35 +0200

    Three words

---
+++ Elementary/06.three_words.py
@@ -0,0 +1,20 @@
+def checkio(words):
+    count = 0
+    for word in words.split():
+        try:
+            int(word)
+            count = 0
+        except:
+            count += 1
+        if count == 3:
+            return True
+    return False
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio(u"Hello World hello") == True, "Hello"
+    assert checkio(u"He is 123 man") == False, "123 man"
+    assert checkio(u"1 2 3 4") == False, "Digits"
+    assert checkio(u"bla bla bla bla") == True, "Bla Bla"
+    assert checkio(u"Hi") == False, "Hi"
+
