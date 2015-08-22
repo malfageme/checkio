7448d605e8b7cbcb5bb020af3a9139f89e7e7a54
Miguel Alfageme <malfageme@gmail.com>
2015-08-22 17:58:53 +0200

    End of other

---
+++ Elementary/12.end_of_other.py
@@ -0,0 +1,11 @@
+def checkio(words_set):
+    return any([check.rfind(word)!=-1 and len(check) - check.rfind(word) == len(word) for check in words_set for word in words_set if check != word])
+    
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio({u"hello", u"lo", u"he"}) == True, "helLO"
+    assert checkio({u"hello", u"la", u"hellow", u"cow"}) == False, "hellow la cow"
+    assert checkio({u"walk", u"duckwalk"}) == True, "duck to walk"
+    assert checkio({u"one"}) == False, "Only One"
+    assert checkio({u"helicopter", u"li", u"he"}) == False, "Only end"
